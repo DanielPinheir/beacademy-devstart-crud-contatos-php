54b22f740c1e31947d7fcee376ad36c12fe44553
DanielPinheir <dyelzim@gmail.com>
2022-06-07 23:58:02 -0300

    upload crud contatos

--- projeto/index.php
+++ projeto/index.php
@@ -1,19 +1,21 @@
 <?php
 
 //verificando a requisicao(o que vem apos o dominio)
-$url = $_SERVER['REQUEST_URI'];
+$url = explode('?', $_SERVER['REQUEST_URI']);
 
 //incluindo outros arquivos
 include 'telas/head.php';
 include 'telas/menu.php';
 include 'acoes.php';
 
-match ($url) {
+match ($url[0]) {
   '/' => home(),
   '/login' => login(),
   '/cadastro' => cadastro(),
   '/listar' => listar(),
   '/relatorio' => relatorio(),
+  '/excluir' => excluir(),
+  '/editar' => editar(),
   default => erro404(),
 };
 
